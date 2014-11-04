bc73f0e38d740bbe059b70ee359bab406198fd99
Koen Vendrik <k.vendrik@gmail.com>
2014-11-04 16:26:03 +0100

    Cleaning up parser, moving to class

--- parse.rb
+++ parse.rb
@@ -12,108 +12,131 @@ require 'rubygems'
 require 'redcarpet'
 require 'fileutils'
 require 'json'
+require 'optparse'
 
 markdown    = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
 
 #Check the folder to obtain .md files from
 read_folder  = ARGV[0]
 
-#defaults
-output      = ''
-cover       = ''
-page_index   = ''
-i           = 0
-defaults    = false
+options = {}
+OptionParser.new do |opts|
+		opts.banner = "Usage: parse.rb [options]"
 
-# iniate the template to copy
-directory = read_folder+'/book-output'
-FileUtils.mkdir_p(directory)
+		opts.on("-v", "--verbose", "Run verbosely") do |v|
+			puts options[:verbose] = v
+		end
+end.parse!
 
 
-FileUtils.cp_r(Dir.glob('template/*'), read_folder+'/book-output')
-set_file = read_folder+'/book-settings.json'
+class MdownToBook
 
+	def initialize(file_path)
+		@output_html = {
+			:base => ''
+			:cover => ''
+			:index => ''
+		}
 
-if File.exist?(set_file)
-    s = File.open(set_file, "rb")
-else
-    s = File.open('default-settings.json', "rb")
-    defaults = true
+		@curr_file_idx = 0
+		@defaults = false
+		@file_path = file_path
+
+		createOutputDir()
+
+		settings = getSettings()
+
+		#read folder and put contens in the right strings and array's
+		Dir.glob(file_path+'/*.md') do |rb_file|
+				puts rb_file
+				file = File.open(rb_file, "rb")
+				if i == 0
+						cover = markdown.render(file.read)
+				else
+						contents = markdown.render(file.read)
+						contents = contents.gsub(/\<img\ ?(\w{0,5})\=\"([^\"]+)\"\ ?(\w{0,5})\=\"([^\"]+)\"\ ?(\w{0,5})\=\"([^\"]+)\"\ ?\/?\>/, "
+																				<div class='enlarge'>
+																						<img src='\\2' alt='\\4'>
+																				</div>
+																				<cite>\\6</cite>")
+						output = output + '<div class="page" id="page-'+i.to_s+'">' + contents + '</div>'
+
+						# compose page index
+						rb_file[read_folder] = ''
+						rb_file['.md'] = ''
+						result = rb_file.gsub(/\A[\d_\W]+|[\d_\W]+\Z/, '\1')
+						page_index += '<tr><td><a href="?page='+i.to_s+'">'+result+'</a></td><td><a href="?page='+i.to_s+'">'+i.to_s+'</a></td></tr>'
+				end
+				i+=1
+		end
+
+		file = File.open(file_path+'/book-output/book/index.html', "rb")
+		temp = file.read
+
+		if !defaults
+				d = File.open('default-settings.json', "rb")
+				defaults = JSON.parse(d.read)
+				if settings['title'].nil?
+						settings['title'] = defaults['title']
+				end
+				 if settings['footer'].nil?
+						settings['footer'] = defaults['footer']
+				end
+				 if settings['buttons']['previous'].nil?
+						settings['buttons']['previous'] = defaults['buttons']['previous']
+				end
+				if settings['buttons']['next'].nil?
+						settings['buttons']['next'] = defaults['buttons']['next']
+				end
+				if settings['contents-table'].nil?
+						settings['contents-table'] = defaults['contents-table']
+				end
+		end
+
+		t_title  = settings['title']
+		t_footer  = settings['footer']
+		t_btnpr  = settings['buttons']['previous']
+		t_btnne  = settings['buttons']['next']
+		t_contents = settings['contents-table']
+
+		temp['{{TITLE}}']   = t_title
+		temp['{{FOOTER}}']  = t_footer
+		temp['{{BUTTONPREV}}']  = t_btnpr
+		temp['{{BUTOTNNEXT}}']  = t_btnne
+
+		cover       = '<section class="page cover">' + cover + '</section>'
+		page_index   = '<section class="page index"><h2>'+t_contents+'</h2><table>' + page_index + '</table></section>'
+
+		temp['{{PAGES}}']   = cover+page_index+output
+
+		outfile = File.new file_path+'/book-output/book/index.html',"w"
+		outfile.puts(temp);
+		outfile.close;
+		puts 'File written to ' + file_path + '/book-output/book';
+	end
+
+private
+
+	def createOutputDir(){
+		# create dir
+		output_dir = @file_path+'/book-output'
+		FileUtils.mkdir_p(output_dir)
+
+		# copy files
+		FileUtils.cp_r(Dir.glob('template/*'), output_dir)
+	}
+
+	def getSettings(){
+		# set config file
+		conf_file = @file_path+'/book-settings.json'
+		if File.exist?(conf_file)
+				s = File.open(conf_file, "rb")
+		else
+				s = File.open('default-settings.json', "rb")
+				defaults = true
+		end
+
+		return JSON.parse(s.read)
+	}
+
 end
-
-settings = JSON.parse(s.read)
-
-#read folder and put contens in the right strings and array's
-Dir.glob(read_folder+'/*.md') do |rb_file|
-    puts rb_file
-    file = File.open(rb_file, "rb")
-    if i == 0
-        cover = markdown.render(file.read)
-    else
-        contents = markdown.render(file.read)
-        contents = contents.gsub(/\<img\ ?(\w{0,5})\=\"([^\"]+)\"\ ?(\w{0,5})\=\"([^\"]+)\"\ ?(\w{0,5})\=\"([^\"]+)\"\ ?\/?\>/, "
-                                    <div class='enlarge'>
-                                        <img src='\\2' alt='\\4'>
-                                    </div>
-                                    <cite>\\6</cite>")
-        output = output + '<div class="page" id="page-'+i.to_s+'">' + contents + '</div>'
-
-        # compose page index
-        rb_file[read_folder] = ''
-        rb_file['.md'] = ''
-        result = rb_file.gsub(/\A[\d_\W]+|[\d_\W]+\Z/, '\1')
-        page_index += '<tr><td><a href="?page='+i.to_s+'">'+result+'</a></td><td><a href="?page='+i.to_s+'">'+i.to_s+'</a></td></tr>'
-    end
-    i+=1
-end
-
-file = File.open(read_folder+'/book-output/book/index.html', "rb")
-temp = file.read
-
-if !defaults
-    d = File.open('default-settings.json', "rb")
-    defaults = JSON.parse(d.read)
-    if settings['title'].nil?
-        settings['title'] = defaults['title']
-    end
-     if settings['footer'].nil?
-        settings['footer'] = defaults['footer']
-    end
-     if settings['buttons']['previous'].nil?
-        settings['buttons']['previous'] = defaults['buttons']['previous']
-    end
-    if settings['buttons']['next'].nil?
-        settings['buttons']['next'] = defaults['buttons']['next']
-    end
-    if settings['contents-table'].nil?
-        settings['contents-table'] = defaults['contents-table']
-    end
-end
-
-t_title  = settings['title']
-t_footer  = settings['footer']
-t_btnpr  = settings['buttons']['previous']
-t_btnne  = settings['buttons']['next']
-t_contents = settings['contents-table']
-
-temp['{{TITLE}}']   = t_title
-temp['{{FOOTER}}']  = t_footer
-temp['{{BUTTONPREV}}']  = t_btnpr
-temp['{{BUTOTNNEXT}}']  = t_btnne
-
-cover       = '<section class="page cover">' + cover + '</section>'
-page_index   = '<section class="page index"><h2>'+t_contents+'</h2><table>' + page_index + '</table></section>'
-
-temp['{{PAGES}}']   = cover+page_index+output
-
-<<<<<<< HEAD:parse.rb
-outfile = File.new read_folder+'/book-output/book/index.html',"w"
-outfile.puts(temp)
-outfile.close
-puts 'File written to ' + read_folder + '/book-output/book'
-=======
-outfile = File.new readFolder+'/book-output/book/index.html',"w"
-outfile.puts(temp);
-outfile.close;
-puts 'File written to ' + readFolder + '/book-output/book';
->>>>>>> FETCH_HEAD:parse.rb
